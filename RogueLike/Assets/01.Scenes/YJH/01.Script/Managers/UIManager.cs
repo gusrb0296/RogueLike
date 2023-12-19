cf2ff9befbabc3cc70671e3f7fee984dab09ff73
goodehd <75984454+goodehd@users.noreply.github.com>
2023-12-19 11:39:18 +0900

    feat : map update
    맵관련 사용할 스크립트 추가
    발판 타일 프리팹들 추가

--- RogueLike/Assets/01.Scenes/YJH/01.Script/Managers/UIManager.cs
+++ RogueLike/Assets/01.Scenes/YJH/01.Script/Managers/UIManager.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class UIManager : MonoBehaviour
-{
-    // 게임 내 UI 관리
+//public class UIManager : MonoBehaviour
+//{
+//    // 게임 내 UI 관리
 
-    // 화면에 표시되는 UI의 초기화, 갠신, 숨김 등을 처리
+//    // 화면에 표시되는 UI의 초기화, 갠신, 숨김 등을 처리
 
-    // 사용자 입력에 따른 이벤트 처리 담당
+//    // 사용자 입력에 따른 이벤트 처리 담당
 
-}
+//}
